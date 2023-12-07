e56a2732089fb3b3af4601e34fa433b1d6476f36
DRV111 <xblackcrownx@gmail.com>
2023-12-07 14:51:39 +0300

    feat: add main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+      </Main>
     </div>
   );
 }
