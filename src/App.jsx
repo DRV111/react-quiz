f138137acc054d171fedb9a8d9fe06edfee0acd6
DRV111 <xblackcrownx@gmail.com>
2023-12-09 11:40:02 +0300

    feat: set ready, error state with useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import { useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Unknown action');
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    async function getQuestions() {
+      try {
+        const response = await fetch('http://localhost:8000/questions');
+        if (!response.ok) {
+          throw new Error('Failed to fetch questions');
+        }
+        const data = await response.json();
+        dispatch({ type: 'dataRecieved', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+    getQuestions();
+  }, []);
   return (
     <div className="app">
       <Header />
